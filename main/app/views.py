536f379838ed2a49e57cfba2659489595d20ddd0
bayramgeldiyevichh <ezizbayramgeldiyevich@gmail.com>
2022-12-15 17:48:28 +0300

    second

--- main/app/views.py
+++ main/app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import *
 
-# Create your views here.
+def index(request):
+    context = {
+
+    }
+
+    return render(request, 'index.html', context)
+
+
+def register(request):
+
+    return render(request, 'login_page.html')
--- main/main/settings.py
+++ main/main/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-g*@jb*j$ib%_ziw^kk(&cygjcoq+_i9=ony=ccp$))8(7(lgl3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [BASE_DIR/'templates']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app'
+    'app',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'main.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,6 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR/'static']
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR/'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
