a3c6d33740d5155da0161bae3814f92c9ca6c061
bayramgeldiyevichh <ezizbayramgeldiyevich@gmail.com>
2022-12-20 12:39:51 +0300

    there

--- main/main/settings.py
+++ main/main/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-g*@jb*j$ib%_ziw^kk(&cygjcoq+_i9=ony=ccp$))8(7(lgl3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.120']
 
 
 # Application definition
